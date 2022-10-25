5cd785471a7989bc7081e9b64df03abc01c0fe1b
Echesa Imaculate Zepwa <imaculate.zepwa@student.moringaschool.com>
2022-10-25 08:47:16 +0300

    complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
-class Person
 
+require 'pry'
+class Person
+    def name=(name)
+        @name = name
+      end
+
+      def job=(job)
+        @job = job
+      end
+    
+      # getter method
+      def name
+        @name
+      end
+
+      def job
+        @job
+      end
+
+     
+     
 end
+kanye = Person.new
+kanye.name = "Kanye"
+binding.pry
